676f690068191fdbe48c9cae6b876c0fed538287
GerryFox22 <gerry.cuomo1994@gmail.com>
2021-10-01 16:15:23 +0200

    formattazione Milestone 1

---
+++ src/components/CardResult.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card-result">
+      <div class="film info">
+          <p><strong>Titolo</strong> : {{film.title}}</p>
+          <p><strong>Titolo Originale</strong> : {{film.original_title}}</p>
+          <p><strong>Lingua</strong> : {{film.original_language}}</p>
+          <p><strong>Voto</strong> : {{film.vote_average}}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name :"CardResult",
+    props : ["film"],
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,21 +1,45 @@
-<template>
-  <div class="Results">
-      {{testoRicercato}}
-  </div>
+<template> 
+    <div class="results-container d-flex p-4">
+        <div v-for="(film,index) in dataFilms" :key="index" class="col">
+            <CardResult :film="film"/>
+        </div>
+    </div> 
 </template>
 
 <script>
+import CardResult from "../components/CardResult.vue"
+import axios from "axios";
 
 export default {
     name :"Results",
     props :["testoRicercato"],
+    
+    components : {
+        CardResult,
+    },
+
+    data : function (){
+        return {
+            dataFilms : [],
+        }
+    },
+
+
+    mounted () {
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=101cc2fb5e4661f4747b2caae74d2081&query="testoRicercato"')
+        .then((response) => {
+
+        this.dataFilms = response.data.results
+        console.log(this.dataFilms);
+    })
+  },
   
 }
 </script>
 
 <style lang="scss" scoped>
-.Results {
+.results-container {
     background-color: gray;
-    height: 50vh;
+
 }
 </style>
